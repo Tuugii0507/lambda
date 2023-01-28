bf351d090e765f17a110fe56d5efdc72d680a2d9
HOP3 Tuguldur Hero <TuguldurHero@NEST-96s-iMac.local>
2023-01-28 13:08:07 +0800

    2023-01-28

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 const { DynamoDB } = require("@aws-sdk/client-dynamodb");
 const { marshall } = require("@aws-sdk/util-dynamodb");
+const { S3 } = require("@aws-sdk/client-s3");
 
 const db = new DynamoDB({
   region: "ap-southeast-1",
 });
 
+const s3 = new S3({
+  region: "ap-southeast-1",
+});
+
 exports.handler = async (event) => {
+  const data = event.body;
   try {
-    await db.getItem({
-      TableName: "users",
-      Key: marshall({ id: 3 }),
-      // Key: { id: { N: "3" } },
+    await s3.putObject({
+      Bucket: "k1bnq2",
+      Key: "973967.jpg",
+      Body: Buffer.from(data, "base64"),
+      ContentType: "image/jpeg",
+      ContentEncoding: "base64",
     });
-    console.log(item);
+
+    // await db.getItem({
+    //   TableName: "users",
+    //   Key: marshall({ id: 3 }),
+    // });
+    // console.log(item);
     // await db.putItem({
     //   Item: { id: { S: "2" }, username: { S: "Tuguldur" } },
     //   TableName: "users",
@@ -30,8 +43,3 @@ exports.handler = async (event) => {
     body: JSON.stringify("Hello from Lambda!"),
   };
 };
-
-//getItem
-//putItem
-//updateItem
-//! try this
